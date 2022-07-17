dec7a50e17d2004a03ae3b082834efbad840e6ed
Jason Morton <jason@jasonmorton.com>
2022-07-17 16:46:56 -0400

    Add i32->felt conversion

---
+++ src/fieldutils.rs
@@ -0,0 +1,26 @@
+use halo2_proofs::{
+    arithmetic::FieldExt,
+};
+
+pub fn i32toF<F: FieldExt> (x: i32) -> F {
+    if x >=0 {
+	F::from(x as u64)
+    } else {
+	-F::from((-x) as u64)
+    }
+}
+
+
+mod test {
+    use super::*;
+    use halo2_proofs::{
+	pasta::Fp as F,
+    };
+    
+    #[test]
+    fn test_conv() {
+	let res: F = i32toF(-15i32);
+	assert_eq!(res,-F::from(15));
+	
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ mod mvmul2;
 mod mvrl;
 mod relu8;
 mod xor;
+mod fieldutils;
