4c3356a126daccb44cd7d49f204a73b1fa5551d6
Jason Morton <jason.morton@gmail.com>
2022-07-19 16:25:06 -0400

    Three layers

---
+++ src/layertrait.rs
@@ -0,0 +1,44 @@
+pub trait TwoLayer {
+    type L0;
+    type L1;
+}
+
+pub trait ThreeLayer {
+    type L0;
+    type L1;
+    type L2;
+}
+
+pub trait FourLayer {
+    type L0;
+    type L1;
+    type L2;
+    type L3;
+}
+
+pub trait FiveLayer {
+    type L0;
+    type L1;
+    type L2;
+    type L3;
+    type L4;
+}
+
+pub trait SixLayer {
+    type L0;
+    type L1;
+    type L2;
+    type L3;
+    type L4;
+    type L5;
+}
+
+pub trait SevenLayer {
+    type L0;
+    type L1;
+    type L2;
+    type L3;
+    type L4;
+    type L5;
+    type L6;
+}
