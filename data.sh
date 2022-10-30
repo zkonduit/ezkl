33a895f781219a9d348a868d55caa71865871271
dcbuilder.eth <dcbuilder@protonmail.com>
2022-10-30 19:03:39 +0000

    add(mnist data): download script for mnist dataset and readme explainer

---
+++ data.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+mkdir data
+cd data
+
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+
+gzip -d *.gz
