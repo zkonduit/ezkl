8a7c1330868d14c34fdcc619a48ee88c14d2eb35
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2022-12-23 12:27:03 +0000

    chore: the great documentation push of 2022

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![deny(warnings, unsafe_code)]
+#![deny(missing_docs, warnings, unsafe_code)]
 #![feature(slice_flatten)]
 
 /// Methods for configuring tensor operations and assigning values to them in a Halo2 circuit.
