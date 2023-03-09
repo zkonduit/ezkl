abf6733f365b7aab38dfe47a4d495113396e694e
dante <45801863+alexander-camuto@users.noreply.github.com>
2023-03-09 17:36:12 +0000

    fix: constant division lookup op (#158)

---
+++ examples/onnx/1l_div/gen.py
@@ -0,0 +1,16 @@
+from torch import nn
+from ezkl import export
+
+class Circuit(nn.Module):
+    def __init__(self, inplace=False):
+        super(Circuit, self).__init__()
+
+    def forward(self, x):
+        return x/ 10
+
+
+circuit = Circuit()
+export(circuit, input_shape = [1])
+
+
+    
