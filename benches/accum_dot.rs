5cd1ecebc38e3cbfee64f27adec5343314c0bc5d
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2023-03-15 00:05:54 +0000

    Update accum_dot.rs

--- benches/accum_dot.rs
+++ benches/accum_dot.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use ezkl_lib::circuit::accumulated_dot::*;
+use ezkl_lib::circuit::accumulated::dot::*;
 use ezkl_lib::commands::TranscriptType;
 use ezkl_lib::execute::create_proof_circuit_kzg;
 use ezkl_lib::pfsys::{create_keys, gen_srs};
