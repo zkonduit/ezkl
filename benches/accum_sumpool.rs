aeeb77e033e808da02dc33fa212c19c028cbaa22
dante <45801863+alexander-camuto@users.noreply.github.com>
2023-03-18 23:37:52 +0000

    Update accum_sumpool.rs

--- benches/accum_sumpool.rs
+++ benches/accum_sumpool.rs
@@ -79,7 +79,7 @@ fn runsumpool(c: &mut Criterion) {
 
     let params = gen_srs::<KZGCommitmentScheme<_>>(K as u32);
 
-    for size in [1, 2, 4].iter() {
+    for size in [1, 2].iter() {
         unsafe {
             IMAGE_HEIGHT = size * 4;
             IMAGE_WIDTH = size * 4;
