3302d5cd39b066e8c809b3abe359ad441b94f8f2
jseam2 <hello.jseam@gmail.com>
2023-05-21 07:10:13 +0800

    uncomment test

--- tests/python/binding_tests.py
+++ tests/python/binding_tests.py
@@ -47,16 +47,16 @@ def test_table_1l_average():
     assert ezkl_lib.table(path) == expected_table
 
 
-# def test_gen_srs():
-#     """
-#     test for gen_srs() with 17 logrows and 20 logrows.
-#     You may want to comment this test as it takes a long time to run
-#     """
-#     ezkl_lib.gen_srs(params_path, 17)
-#     assert os.path.isfile(params_path)
+def test_gen_srs():
+    """
+    test for gen_srs() with 17 logrows and 20 logrows.
+    You may want to comment this test as it takes a long time to run
+    """
+    ezkl_lib.gen_srs(params_path, 17)
+    assert os.path.isfile(params_path)
 
-#     ezkl_lib.gen_srs(params_k20_path, 20)
-#     assert os.path.isfile(params_k20_path)
+    ezkl_lib.gen_srs(params_k20_path, 20)
+    assert os.path.isfile(params_k20_path)
 
 
 def test_forward():
