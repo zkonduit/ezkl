4d4dfc30cc1c3c6d96f4b1f148bc106da58c4430
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2023-05-23 15:38:53 +0100

    cleanup self attention

--- examples/onnx/self_attention/gen.py
+++ examples/onnx/self_attention/gen.py
@@ -44,17 +44,13 @@ class CausalSelfAttention(nn.Module):
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.dropout = config.dropout
-        # # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-        # self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
-        # if not self.flash:
-        #     print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
-        #     # causal mask to ensure that attention is only applied to the left in the input sequence
+
+        # causal mask to ensure that attention is only applied to the left in the input sequence
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                         .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
@@ -63,7 +59,7 @@ class CausalSelfAttention(nn.Module):
 
         # manual implementation of attention
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) #q shape:(B, nh, T, hs), k transpose shape (B, nh, hs, T) -> (B, nh, T, T)
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, 0.)
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float(-10))
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
