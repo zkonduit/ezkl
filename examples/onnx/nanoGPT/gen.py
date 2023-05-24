b0cd251b59c39cd29c0f43c1c833faec57963906
Alexander Camuto <45801863+alexander-camuto@users.noreply.github.com>
2023-05-24 21:58:23 +0100

    4m

--- examples/onnx/nanoGPT/gen.py
+++ examples/onnx/nanoGPT/gen.py
@@ -4,6 +4,7 @@
 Reference: https://github.com/karpathy/nanoGPT
 """
 
+import json
 import math
 from dataclasses import dataclass
 import torch
@@ -14,8 +15,7 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-import torch
-import json
+
 
 def new_gelu(x):
     """
@@ -23,6 +23,7 @@ def new_gelu(x):
     Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
     """
     return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * x * x * x)))
+
 
 class LayerNorm(nn.Module):
     """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
@@ -35,13 +36,15 @@ class LayerNorm(nn.Module):
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
 
+
 class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        self.c_attn = nn.Linear(
+            config.n_embd, 3 * config.n_embd, bias=config.bias)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         # regularization
@@ -53,34 +56,41 @@ class CausalSelfAttention(nn.Module):
 
         # causal mask to ensure that attention is only applied to the left in the input sequence
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                        .view(1, 1, config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        B, T, C = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C //
+                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C //
+                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C //
+                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
 
         # manual implementation of attention
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) #q shape:(B, nh, T, hs), k transpose shape (B, nh, hs, T) -> (B, nh, T, T)
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float(-10))
+        # q shape:(B, nh, T, hs), k transpose shape (B, nh, hs, T) -> (B, nh, T, T)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float(-10))
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
 
         y = self.resid_dropout(self.c_proj(y))
         return y
-    
-    
+
+
 class MLP(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.c_fc = nn.Linear(
+            config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.c_proj = nn.Linear(
+            4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
@@ -89,6 +99,7 @@ class MLP(nn.Module):
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
+
 
 class Block(nn.Module):
 
@@ -104,15 +115,19 @@ class Block(nn.Module):
         x = x + self.mlp(self.ln_2(x))
         return x
 
+
 @dataclass
 class GPTConfig:
     block_size: int = 1024
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    vocab_size: int = 50304
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
     dropout: float = 0.0
-    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    bias: bool = True
+
 
 class GPT(nn.Module):
 
@@ -123,28 +138,29 @@ class GPT(nn.Module):
         self.config = config
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
-            drop = nn.Dropout(config.dropout),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+            wte=nn.Embedding(config.vocab_size, config.n_embd),
+            wpe=nn.Embedding(config.block_size, config.n_embd),
+            drop=nn.Dropout(config.dropout),
+            h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f=LayerNorm(config.n_embd, bias=config.bias),
         ))
-        
+
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         # weight-tying
-        self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
+        # https://paperswithcode.com/method/weight-tying
+        self.transformer.wte.weight = self.lm_head.weight
         self.block = Block(config)
         # init all weights
         self.apply(self._init_weights)
         # apply special scaled init to the residual projections, per GPT-2 paper
         for pn, p in self.named_parameters():
             if pn.endswith('c_proj.weight'):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
 
         # report number of parameters
         print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
-
 
     def get_num_params(self, non_embedding=True):
         """
@@ -170,46 +186,49 @@ class GPT(nn.Module):
         device = idx.device
         b, t = idx.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
-        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0) # shape (1, t)
-    
+        pos = torch.arange(0, t, dtype=torch.long,
+                           device=device).unsqueeze(0)  # shape (1, t)
+
         # # # forward the GPT model itself
-        idx =  self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd), idx -> token_emb
-        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (1, t, n_embd)
+        # token embeddings of shape (b, t, n_embd), idx -> token_emb
+        idx = self.transformer.wte(idx)
+        # position embeddings of shape (1, t, n_embd)
+        pos_emb = self.transformer.wpe(pos)
         idx = self.transformer.drop(idx + pos_emb)
 
         for block in self.transformer.h:
             idx = block(idx)
-        
+
         idx = self.transformer.ln_f(idx)
         idx = self.lm_head(idx)
 
         return idx
 
 
-gptconf = GPTConfig(block_size = 64, vocab_size = 65, n_layer = 20, n_head = 4, n_embd = 64,dropout = 0.0, bias = False) 
+gptconf = GPTConfig(block_size=64, vocab_size=65, n_layer=80,
+                    n_head=4, n_embd=64, dropout=0.0, bias=False)
 model = GPT(gptconf)
 model.get_num_params()
 
 
 shape = [1, 64]
-x =  torch.randint(65, (1, 64))
+x = torch.randint(65, (1, 64))
 torch_out = model(x)
 
 torch.onnx.export(model, x, "network.onnx",
-       export_params=True,        # store the trained parameter weights inside the model file
-        opset_version=10,          # the ONNX version to export the model to
-        do_constant_folding=True,  # whether to execute constant folding for optimization
-        input_names = ['input'],   # the model's input names
-        output_names = ['output'], # the model's output names
-        dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
-        'output' : {0 : 'batch_size'}})
+                  export_params=True,        # store the trained parameter weights inside the model file
+                  opset_version=10,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names=['input'],   # the model's input names
+                  output_names=['output'],  # the model's output names
+                  dynamic_axes={'input': {0: 'batch_size'},    # variable length axes
+                                'output': {0: 'batch_size'}})
 
 d = ((x).detach().numpy()).reshape([-1]).tolist()
 
-data = dict(input_shapes = [shape],
-            input_data = [d],
-            output_data = [((o).detach().numpy()).reshape([-1]).tolist() for o in torch_out])
+data = dict(input_shapes=[shape],
+            input_data=[d],
+            output_data=[((o).detach().numpy()).reshape([-1]).tolist() for o in torch_out])
 
 # Serialize data into file:
-json.dump( data, open( "input.json", 'w' ) )
-
+json.dump(data, open("input.json", 'w'))
