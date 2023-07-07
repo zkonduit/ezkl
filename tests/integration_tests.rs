22e186881a28e3c2384dfc4a341ee45d3fd3aeaa
jseam2 <hello.jseam@gmail.com>
2023-07-07 20:13:10 +0800

    refactor instance of ezkl_lib to ezkl

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -3,8 +3,8 @@
 mod native_tests {
 
     use core::panic;
-    use ezkl_lib::graph::input::{FileSource, GraphInput};
-    use ezkl_lib::graph::DataSource;
+    use ezkl::graph::input::{FileSource, GraphInput};
+    use ezkl::graph::DataSource;
     use lazy_static::lazy_static;
     use std::env::var;
     use std::process::Command;
