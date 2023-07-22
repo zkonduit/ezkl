6074f70f2ee7066716ad885deba72879847888c8
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2023-07-22 10:43:33 +0100

    small patch

--- src/execute.rs
+++ src/execute.rs
@@ -696,7 +696,7 @@ pub(crate) async fn calibrate(
                 res.push(task);
             }
         }
-        if let Some(best) = res.into_iter().max_by_key(|p| p.run_args.logrows) {
+        if let Some(best) = res.into_iter().max_by_key(|p| p.run_args.bits) {
             // pick the one with the largest logrows
             found_params.push(best);
         }
