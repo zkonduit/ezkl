2be89e0d78712c8356c407346f069607c87ebc41
jseam2 <hello.jseam@gmail.com>
2023-08-10 07:29:14 +0800

    test: update get_srs test

--- tests/python/binding_tests.py
+++ tests/python/binding_tests.py
@@ -207,6 +207,13 @@ def test_get_srs():
 
     assert os.path.isfile(srs_path)
 
+    override_srs_path = os.path.join(folder_path, "kzg_test_k8.params")
+
+    res = ezkl.get_srs(srs_path, logrows=8)
+
+    assert os.path.isfile(override_srs_path)
+
+
 
 def test_mock():
     """
