b943ce5117c1a09505bc537f78350ed486716306
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2023-10-27 17:45:48 +0100

    Create gen.py

---
+++ examples/onnx/rounding_ops/gen.py
@@ -0,0 +1,55 @@
+import io
+import numpy as np
+from torch import nn
+import torch.onnx
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import json
+
+
+class Circuit(nn.Module):
+    def __init__(self):
+        super(Circuit, self).__init__()
+
+    def forward(self, w, x, y):
+
+        return torch.round(w), torch.floor(x), torch.ceil(y)
+
+
+def main():
+    torch_model = Circuit()
+    # Input to the model
+    shape = [3, 2, 3]
+    w = 0.1*torch.rand(1, *shape, requires_grad=True)
+    x = 0.1*torch.rand(1, *shape, requires_grad=True)
+    y = 0.1*torch.rand(1, *shape, requires_grad=True)
+    torch_out = torch_model(w, x, y)
+    # Export the model
+    torch.onnx.export(torch_model,               # model being run
+                      # model input (or a tuple for multiple inputs)
+                      (w, x, y),
+                      # where to save the model (can be a file or file-like object)
+                      "network.onnx",
+                      export_params=True,        # store the trained parameter weights inside the model file
+                      opset_version=16,          # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names=['input'],   # the model's input names
+                      output_names=['output'],  # the model's output names
+                      dynamic_axes={'input': {0: 'batch_size'},    # variable length axes
+                                    'output': {0: 'batch_size'}})
+
+    dw = ((w).detach().numpy()).reshape([-1]).tolist()
+    dx = ((x).detach().numpy()).reshape([-1]).tolist()
+    dy = ((y).detach().numpy()).reshape([-1]).tolist()
+
+    data = dict(input_shapes=[shape, shape, shape, shape],
+                input_data=[dw, dx, dy],
+                output_data=[((o).detach().numpy()).reshape([-1]).tolist() for o in torch_out])
+
+    # Serialize data into file:
+    json.dump(data, open("input.json", 'w'))
+
+
+if __name__ == "__main__":
+    main()
