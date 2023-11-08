9529a32c41d60e15b59f5e0e5c054d391c9c4684
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2023-11-08 14:27:05 +0000

    Update integration_tests.rs

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2399,6 +2399,7 @@ mod native_tests {
                 &format!("--input-visibility={}", input_visibility),
                 &format!("--param-visibility={}", param_visibility),
                 &format!("--output-visibility={}", output_visibility),
+                &format!("--num-inner-cols={}", 1),
             ])
             .status()
             .expect("failed to execute process");
