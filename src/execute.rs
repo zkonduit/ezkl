244906f60c6bbe7a4e9d82a0da7c523c5ef49dae
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2024-02-01 21:03:32 +0000

    Update execute.rs

--- src/execute.rs
+++ src/execute.rs
@@ -735,23 +735,23 @@ impl AccuracyResults {
             abs_percentage_errors.iter().sum::<f32>() / abs_percentage_errors.len() as f32;
         let mean_error = errors.iter().sum::<f32>() / errors.len() as f32;
         let median_error = errors[errors.len() / 2];
-        let max_error = errors
-            .into_iter()
+        let max_error = *errors
+            .iter()
             .max_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal))
             .unwrap();
-        let min_error = errors
-            .into_iter()
+        let min_error = *errors
+            .iter()
             .min_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal))
             .unwrap();
 
         let mean_abs_error = abs_errors.iter().sum::<f32>() / abs_errors.len() as f32;
         let median_abs_error = abs_errors[abs_errors.len() / 2];
-        let max_abs_error = abs_errors
-            .into_iter()
+        let max_abs_error = *abs_errors
+            .iter()
             .max_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal))
             .unwrap();
-        let min_abs_error = abs_errors
-            .into_iter()
+        let min_abs_error = *abs_errors
+            .iter()
             .min_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal))
             .unwrap();
 
