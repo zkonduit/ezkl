09912ea0a13df5fe01e6badb907206a14e857ec8
dante <45801863+alexander-camuto@users.noreply.github.com>
2024-03-26 19:00:52 +0000

    Update layouts.rs

--- src/circuit/ops/layouts.rs
+++ src/circuit/ops/layouts.rs
@@ -655,7 +655,7 @@ fn select<F: PrimeField + TensorType + PartialOrd + std::hash::Hash>(
         let felt_evals = input.get_felt_evals()?;
         index
             .get_int_evals()?
-            .iter()
+            .par_iter()
             .map(|x| Value::known(felt_evals.get(&[*x as usize])))
             .collect::<Tensor<Value<F>>>()
     } else {
