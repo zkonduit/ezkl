f5f8ef56f7b1bb75109eec2e2743182e195d0b84
dante <45801863+alexander-camuto@users.noreply.github.com>
2024-06-07 10:30:21 -0400

    chore: ezkl self update (#809)

--- src/commands.rs
+++ src/commands.rs
@@ -868,6 +868,12 @@ pub enum Commands {
         #[arg(long, value_hint = clap::ValueHint::Other)]
         addr_vk: Option<H160Flag>,
     },
+    /// Updates ezkl binary to version specified (or latest if not specified)
+    Update {
+        /// The version to update to
+        #[arg(value_hint = clap::ValueHint::Other, short='v', long)]
+        version: Option<String>,
+    },
 }
 
 
