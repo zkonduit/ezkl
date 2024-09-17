654707220193a06219daea1245dae6e5bd17422f
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-09-17 18:50:34 +0100

    (feat: bindings) Revised logic to reduce the code complexity and share more logic between wasm and universal bindings

--- src/bindings/mod.rs
+++ src/bindings/mod.rs
@@ -1,7 +1,5 @@
-mod logic;
+/// Universal bindings for all platforms
+pub mod universal;
 /// wasm prover and verifier
 #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
 pub mod wasm;
-/// Universal bindings for all platforms
-#[cfg(target_os = "ios")]
-pub mod universal;
