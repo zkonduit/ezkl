8f8370e0c8610ae5e4904b12aee05ea9d887d1a3
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-10-16 13:36:03 +0700

    (chore: cargo) removed redundant cfg statements

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,3 @@
-#[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
 use alloy::primitives::Address as H160;
 use clap::{Command, Parser, Subcommand};
 use clap_complete::{generate, Generator, Shell};
@@ -17,7 +16,6 @@ use tosubcommand::{ToFlags, ToSubcommand};
 use crate::{pfsys::ProofType, Commitments, RunArgs};
 
 use crate::circuit::CheckMode;
-#[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
 use crate::graph::TestDataSource;
 use crate::pfsys::TranscriptType;
 
@@ -244,28 +242,24 @@ impl From<&str> for ContractType {
 }
 
 
-#[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
 #[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, PartialOrd)]
 /// wrapper for H160 to make it easy to parse into flag vals
 pub struct H160Flag {
     inner: H160,
 }
 
-#[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
 impl From<H160Flag> for H160 {
     fn from(val: H160Flag) -> H160 {
         val.inner
     }
 }
 
-#[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
 impl ToFlags for H160Flag {
     fn to_flags(&self) -> Vec<String> {
         vec![format!("{:#x}", self.inner)]
     }
 }
 
-#[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
 impl From<&str> for H160Flag {
     fn from(s: &str) -> Self {
         Self {
@@ -461,8 +455,7 @@ pub enum Commands {
     },
 
     /// Calibrates the proving scale, lookup bits and logrows from a circuit settings file.
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    CalibrateSettings {
+        CalibrateSettings {
         /// The path to the .json calibration data file.
         #[arg(short = 'D', long, default_value = DEFAULT_CALIBRATION_FILE, value_hint = clap::ValueHint::FilePath)]
         data: Option<PathBuf>,
@@ -512,8 +505,7 @@ pub enum Commands {
         commitment: Option<Commitments>,
     },
 
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Gets an SRS from a circuit settings file.
+        /// Gets an SRS from a circuit settings file.
     #[command(name = "get-srs")]
     GetSrs {
         /// The path to output the desired srs file, if set to None will save to $EZKL_REPO_PATH/srs
@@ -648,8 +640,7 @@ pub enum Commands {
         #[arg(long, default_value = DEFAULT_DISABLE_SELECTOR_COMPRESSION, action = clap::ArgAction::SetTrue)]
         disable_selector_compression: Option<bool>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Deploys a test contact that the data attester reads from and creates a data attestation formatted input.json file that contains call data information
+        /// Deploys a test contact that the data attester reads from and creates a data attestation formatted input.json file that contains call data information
     #[command(arg_required_else_help = true)]
     SetupTestEvmData {
         /// The path to the .json data file, which should include both the network input (possibly private) and the network output (public input to the proof)
@@ -673,8 +664,7 @@ pub enum Commands {
         #[arg(long, default_value = "on-chain", value_hint = clap::ValueHint::Other)]
         output_source: TestDataSource,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// The Data Attestation Verifier contract stores the account calls to fetch data to feed into ezkl. This call data can be updated by an admin account. This tests that admin account is able to update this call data.
+        /// The Data Attestation Verifier contract stores the account calls to fetch data to feed into ezkl. This call data can be updated by an admin account. This tests that admin account is able to update this call data.
     #[command(arg_required_else_help = true)]
     TestUpdateAccountCalls {
         /// The path to the verifier contract's address
@@ -687,8 +677,7 @@ pub enum Commands {
         #[arg(short = 'U', long, value_hint = clap::ValueHint::Url)]
         rpc_url: Option<String>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Swaps the positions in the transcript that correspond to commitments
+        /// Swaps the positions in the transcript that correspond to commitments
     SwapProofCommitments {
         /// The path to the proof file
         #[arg(short = 'P', long, default_value = DEFAULT_PROOF, value_hint = clap::ValueHint::FilePath)]
@@ -698,8 +687,7 @@ pub enum Commands {
         witness_path: Option<PathBuf>,
     },
 
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Loads model, data, and creates proof
+        /// Loads model, data, and creates proof
     Prove {
         /// The path to the .json witness file (generated using the gen-witness command)
         #[arg(short = 'W', long, default_value = DEFAULT_WITNESS, value_hint = clap::ValueHint::FilePath)]
@@ -729,8 +717,7 @@ pub enum Commands {
         #[arg(long, default_value = DEFAULT_CHECKMODE, value_hint = clap::ValueHint::Other)]
         check_mode: Option<CheckMode>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Encodes a proof into evm calldata
+        /// Encodes a proof into evm calldata
     #[command(name = "encode-evm-calldata")]
     EncodeEvmCalldata {
         /// The path to the proof file (generated using the prove command)
@@ -743,8 +730,7 @@ pub enum Commands {
         #[arg(long, value_hint = clap::ValueHint::Other)]
         addr_vk: Option<H160Flag>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Creates an Evm verifier for a single proof
+        /// Creates an Evm verifier for a single proof
     #[command(name = "create-evm-verifier")]
     CreateEvmVerifier {
         /// The path to SRS, if None will use $EZKL_REPO_PATH/srs/kzg{logrows}.srs
@@ -766,8 +752,7 @@ pub enum Commands {
         #[arg(long, default_value = DEFAULT_RENDER_REUSABLE, action = clap::ArgAction::SetTrue)]
         reusable: Option<bool>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Creates an Evm verifier artifact for a single proof to be used by the reusable verifier
+        /// Creates an Evm verifier artifact for a single proof to be used by the reusable verifier
     #[command(name = "create-evm-vka")]
     CreateEvmVKArtifact {
         /// The path to SRS, if None will use $EZKL_REPO_PATH/srs/kzg{logrows}.srs
@@ -786,8 +771,7 @@ pub enum Commands {
         #[arg(long, default_value = DEFAULT_VK_ABI, value_hint = clap::ValueHint::FilePath)]
         abi_path: Option<PathBuf>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Creates an Evm verifier that attests to on-chain inputs for a single proof
+        /// Creates an Evm verifier that attests to on-chain inputs for a single proof
     #[command(name = "create-evm-da")]
     CreateEvmDataAttestation {
         /// The path to load circuit settings .json file from (generated using the gen-settings command)
@@ -811,8 +795,7 @@ pub enum Commands {
         witness: Option<PathBuf>,
     },
 
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Creates an Evm verifier for an aggregate proof
+        /// Creates an Evm verifier for an aggregate proof
     #[command(name = "create-evm-verifier-aggr")]
     CreateEvmVerifierAggr {
         /// The path to SRS, if None will use $EZKL_REPO_PATH/srs/kzg{logrows}.srs
@@ -876,8 +859,7 @@ pub enum Commands {
         #[arg(long, default_value = DEFAULT_COMMITMENT, value_hint = clap::ValueHint::Other)]
         commitment: Option<Commitments>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Deploys an evm contract (verifier, reusable verifier, or vk artifact) that is generated by ezkl
+        /// Deploys an evm contract (verifier, reusable verifier, or vk artifact) that is generated by ezkl
     DeployEvm {
         /// The path to the Solidity code (generated using the create-evm-verifier command)
         #[arg(long, default_value = DEFAULT_SOL_CODE, value_hint = clap::ValueHint::FilePath)]
@@ -898,8 +880,7 @@ pub enum Commands {
         #[arg(long = "contract-type", short = 'C', default_value = DEFAULT_CONTRACT_DEPLOYMENT_TYPE, value_hint = clap::ValueHint::Other)]
         contract: ContractType,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Deploys an evm verifier that allows for data attestation
+        /// Deploys an evm verifier that allows for data attestation
     #[command(name = "deploy-evm-da")]
     DeployEvmDataAttestation {
         /// The path to the .json data file, which should include both the network input (possibly private) and the network output (public input to the proof)
@@ -924,8 +905,7 @@ pub enum Commands {
         #[arg(short = 'P', long, value_hint = clap::ValueHint::Other)]
         private_key: Option<String>,
     },
-    #[cfg(not(any(not(feature = "ezkl"), target_arch = "wasm32")))]
-    /// Verifies a proof using a local Evm executor, returning accept or reject
+        /// Verifies a proof using a local Evm executor, returning accept or reject
     #[command(name = "verify-evm")]
     VerifyEvm {
         /// The path to the proof file (generated using the prove command)
