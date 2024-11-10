12d59fec99a0bc7834fc96a70f83c36319f7e85c
dante <45801863+alexander-camuto@users.noreply.github.com>
2024-11-10 00:38:58 +0000

    Update layouts.rs

--- src/circuit/ops/layouts.rs
+++ src/circuit/ops/layouts.rs
@@ -121,17 +121,6 @@ pub fn diff_less_than<F: PrimeField + TensorType + PartialOrd + std::hash::Hash>
     Ok(())
 }
 
-fn is_positive<F: PrimeField + TensorType + PartialOrd + std::hash::Hash>(
-    config: &BaseConfig<F>,
-    region: &mut RegionCtx<F>,
-    value: &[ValTensor<F>; 1],
-) -> Result<ValTensor<F>, CircuitError> {
-    let neg_one = create_constant_tensor(integer_rep_to_felt(-1), 1);
-    let is_negative = equals(config, region, &[value[0].clone(), neg_one])?;
-
-    not(config, region, &[is_negative])
-}
-
 /// Div accumulated layout
 pub(crate) fn div<F: PrimeField + TensorType + PartialOrd + std::hash::Hash>(
     config: &BaseConfig<F>,
@@ -314,11 +303,8 @@ pub fn sqrt<F: PrimeField + TensorType + PartialOrd + std::hash::Hash>(
     let claimed_output = region.assign(&config.custom_gates.output, &claimed_output)?;
     region.increment(claimed_output.len());
 
-    // assert value is positive
-    let is_positive = is_positive(config, region, &[claimed_output.clone()])?;
-    let ones = create_constant_tensor(F::ONE, is_positive.len());
-    // assert the sign is positive
-    enforce_equality(config, region, &[is_positive, ones])?;
+    // force the output to be positive or zero
+    let claimed_output = abs(config, region, &[claimed_output.clone()])?;
 
     // rescaled input
     let rescaled_input = pairwise(config, region, &[input.clone(), unit_scale], BaseOp::Mult)?;
