8e56094bb5aa3c2077b86ba5c08a48fc6a72066a
dante <45801863+alexander-camuto@users.noreply.github.com>
2024-12-05 10:34:26 -0500

    new stub

--- src/bin/py_stub_gen.rs
+++ src/bin/py_stub_gen.rs
@@ -3,6 +3,7 @@ use pyo3_stub_gen::Result;
 fn main() -> Result<()> {
     // `stub_info` is a function defined by `define_stub_info_gatherer!` macro.
     env_logger::Builder::from_env(env_logger::Env::default().filter_or("RUST_LOG", "info")).init();
-    let stub = mixed_sub::stub_info()?;
+    let stub = ezkl::bindings::python::stub_info()?;
+    stub.generate()?;
     Ok(())
 }
