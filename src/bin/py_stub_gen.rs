5c059e39729df48a7c58a40df3328735facaceb6
dante <45801863+alexander-camuto@users.noreply.github.com>
2024-12-05 01:27:49 -0500

    chore: include py_stub_gen

---
+++ src/bin/py_stub_gen.rs
@@ -0,0 +1,8 @@
+use pyo3_stub_gen::Result;
+
+fn main() -> Result<()> {
+    // `stub_info` is a function defined by `define_stub_info_gatherer!` macro.
+    env_logger::Builder::from_env(env_logger::Env::default().filter_or("RUST_LOG", "info")).init();
+    let stub = mixed_sub::stub_info()?;
+    Ok(())
+}
