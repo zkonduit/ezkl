c6e541de226026de861a5d7e7475e397a919da83
Alexander Camuto <45801863+alexander-camuto@users.noreply.github.com>
2025-02-04 14:05:55 +0000

    Update ops.rs

--- src/tensor/ops.rs
+++ src/tensor/ops.rs
@@ -2250,7 +2250,7 @@ pub mod nonlinearities {
     pub fn const_div(a: &Tensor<IntegerRep>, denom: f64) -> Tensor<IntegerRep> {
         a.par_enum_map(|_, a_i| {
             let d_inv_x = (a_i as f64) / (denom);
-            Ok::<_, TensorError>(d_inv_x.floor() as IntegerRep)
+            Ok::<_, TensorError>(d_inv_x.round() as IntegerRep)
         })
         .unwrap()
     }
