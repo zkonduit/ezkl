911b7032d097606ac3a1ec793bd002c743868e7a
dante <45801863+alexander-camuto@users.noreply.github.com>
2025-02-06 13:20:39 -0500

    Update cat_and_dog_data.sh

--- examples/notebooks/cat_and_dog_data.sh
+++ examples/notebooks/cat_and_dog_data.sh
@@ -1,7 +1,7 @@
 # download tess data
 # check if first argument has been set 
 if [ ! -z "$1" ]; then
-    DATA_DIR=$1
+    DATA_DIR=$1/data
 else
     DATA_DIR=data
 fi
@@ -10,3 +10,4 @@ echo "Downloading data to $DATA_DIR"
 
 if [ ! -d "$DATA_DIR/CATDOG" ]; then
     kaggle datasets download tongpython/cat-and-dog -p $DATA_DIR/CATDOG --unzip
+fi
