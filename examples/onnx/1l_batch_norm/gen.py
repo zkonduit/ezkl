0ce9c280580f29735fe84e24fc57fb7f402138cf
jseam2 <hello.jseam@gmail.com>
2025-03-31 15:43:21 +0800

    fix: migrate 1l_batch_norm export

--- examples/onnx/1l_batch_norm/gen.py
+++ examples/onnx/1l_batch_norm/gen.py
@@ -1,5 +1,6 @@
 from torch import nn
-from ezkl import export
+import torch
+import json
 
 
 class Model(nn.Module):
@@ -12,4 +13,33 @@ class Model(nn.Module):
 
 
 circuit = Model()
-export(circuit, input_shape=[3, 2, 2])
+circuit.eval()
+
+input_shape = [3, 2, 2]
+x = 0.1 * torch.rand(1,*input_shape, requires_grad=True)
+torch_out = circuit(x)
+
+data = {
+    "input_data": [((x).detach().numpy()).reshape([-1]).tolist()],
+    "output_data": [((torch_out).detach().numpy()).reshape([-1]).tolist()]
+}
+
+# Export input.json
+with open("input.json", "w") as f:
+    json.dump(data, f)
+
+# Export network.onnx
+torch.onnx.export(
+    circuit,
+    x,
+    "network.onnx",
+    export_params=True,
+    opset_version=10,
+    do_constant_folding=True,
+    input_names = ['input'],
+    output_names = ['output'],
+    dynamic_axes={
+        'input' : {0 : 'batch_size'},
+        'output' : {0 : 'batch_size'}
+    }
+)
